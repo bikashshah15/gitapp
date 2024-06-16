ca2d24634ae2f237b63e5cecc8249904188dec54
Bikash Shah <bikashshah15@gmail.com>
2024-06-16 11:20:15 -0500

    Code added

---
+++ src/test/java/com/gitapp/GitappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
